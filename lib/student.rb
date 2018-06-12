c609e17da328b3a4ee4dfd688be888459cf0dfab
Conor Hamilton <conhamil19@gmail.com>
2018-06-12 02:46:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,3 +24,21 @@ class Student
   end
 end
 
+class Umbrella::Weather
+  attr_accessor :temperature, :wind, :sunrise, :sunset, :weather_condition, :rain_perc
+  
+  
+  def self.weather_setter
+    
+    doc = Nokogiri::HTML(open("https://weather.com/weather/today/l/Chicago+IL+USIL0225:1:US"))
+    
+    rain = self.new 
+    
+    rain.temperature = doc.search("div.today_nowcard-temp").text
+    rain.weather_condition = doc.search("div.today_nowcard-phrase").text 
+    rain.wind = doc.search("div.today_nowcard-sidecar").search("td")[0].text
+    rain.sunrise = doc.search("span.wx-dsxdate")[0].text
+    rain.sunset = doc.search("span.wx-dsxdate")[1].text
+    rain.rain_perc = doc.search("span.precip-val").first.text
+  
+  end 
